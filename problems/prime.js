7b61176101ecd9d148611ab132104580b64ab787
Lochan Chhetri <lochan.chhetri@gmail.com>
2014-12-13 19:34:16 -0800

    Prime Numbers & Prime Factors

--- problems/prime.js
+++ problems/prime.js
@@ -1,8 +1,19 @@
-/*
+/* Check if a number is a Prime Number */
 
-Find series of Prime Number
+function isPrime(num){
 
-*/
+var isPrime = true;
+
+for(var i = 2; i < num; i++){
+    if ( num%i == 0) isPrime = false;
+}
+
+return isPrime;
+}
+
+isPrime(7);
+
+/* Find series of Prime Number */
 
 function primeNumber(num){
     
@@ -27,3 +38,34 @@ function primeNumber(num){
 }
 
 primeNumber(30);
+
+/* Find Prime Factors */
+
+function primeFactors(num){
+    
+    var check = num;
+    var isCheckPrime = true;
+    var factorsArray = [];
+    var factorsArrayIndex = 0;
+    
+    for(var i = 2; i < num; i++){
+    
+        if( check%i == 0){
+            
+            do{
+                check = check/i;
+                isCheckPrime = isPrime(check);
+                factorsArray[factorsArrayIndex] = i;
+                factorsArrayIndex++;
+            }
+            while( check%i == 0)
+        
+        }
+        
+    }
+    
+        return factorsArray;
+}
+
+primeFactors(13195);
+
